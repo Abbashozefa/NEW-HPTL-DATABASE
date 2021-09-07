3805cf674c0439547d0c8cda5464499c80b07e5c
Abbashozefa <72591326+Abbashozefa@users.noreply.github.com>
2021-09-07 23:58:13 +0530

    Update CODE.py

--- HOSPITAL NEW DATABASE/CODE.py
+++ HOSPITAL NEW DATABASE/CODE.py
@@ -1,10 +1,14 @@
 import tkinter as tk
+
+
 m=tk.Tk()
-
-
-
-
-
-
-
+m.title('HOSPITAL DATABASE')
+L1=Label(m,text='ENTER PATIENT NAME')
+L1.grid(row=0)
+L2=Label(m,text='ENTER DOB')
+L2.grid(row=1)
+E1=Entry(m)
+E1.grid(row=0,column=1)
+E2=Entry(m)
+E2.grid(row=1,column=1)
 m.mainloop()
