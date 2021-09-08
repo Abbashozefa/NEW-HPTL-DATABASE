a962a2a5a0474e6265e46c44ee3cc06da80d9fa7
Abbashozefa <72591326+Abbashozefa@users.noreply.github.com>
2021-09-08 12:49:32 +0530

    Update CODE.py

--- HOSPITAL NEW DATABASE/CODE.py
+++ HOSPITAL NEW DATABASE/CODE.py
@@ -1,14 +1,22 @@
-import tkinter as tk
+from tkinter import *
 
 
-m=tk.Tk()
+m=Tk()
 m.title('HOSPITAL DATABASE')
 L1=Label(m,text='ENTER PATIENT NAME')
 L1.grid(row=0)
 L2=Label(m,text='ENTER DOB')
 L2.grid(row=1)
-E1=Entry(m)
-E1.grid(row=0,column=1)
+def printing():
+    name=e1.get()
+    Label(m,text=name).grid(row=3,column=1)
+e1=Entry(m)
+e1.grid(row=0,column=1)
+
 E2=Entry(m)
 E2.grid(row=1,column=1)
+Button(m,text='yes',command=printing).grid(row=2,column=1)
+
+
+
 m.mainloop()
