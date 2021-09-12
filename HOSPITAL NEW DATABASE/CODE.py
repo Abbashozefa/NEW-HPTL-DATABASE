70007a5df1f7b52ca88294e4964930213ef890a3
Abbashozefa <72591326+Abbashozefa@users.noreply.github.com>
2021-09-12 10:39:59 +0530

    Update CODE.py

--- HOSPITAL NEW DATABASE/CODE.py
+++ HOSPITAL NEW DATABASE/CODE.py
@@ -1,17 +1,16 @@
 from tkinter import *
 import pymysql
 import datetime
+import pandas as pd
 conn=pymysql.connect(host='localhost',user='root',password='yellowyellow',database='EYEHOSPITAL')
 a=conn.cursor()
 m=Tk()
 m.title('HOSPITAL DATABASE')
-def ets():
-    E1=e.get
-    print(E1)
 
-e=Entry(m).grid(row=0)
-Button(m,text="yes",command=ets).grid(row=1)
-
+user=int()
+age=int()
+name=str()
+doc=str()
 
 
 
@@ -19,82 +18,77 @@ Button(m,text="yes",command=ets).grid(row=1)
 
 def receptionist():
                 def send():
-                    pass
+                    user1=user.get()
+                    name1=name.get()
+                    age1=age.get()
+                    doc1=doc.get()
+                    a.execute("insert into PATIENT values("+str(user1)+",'"+name1+"',"+str(age1)+",'"+str(datetime.date.today())+"','"+doc1+"',NULL,NULL,NULL,NULL,NULL,NULL)")
+                    
+                    
                 def newp():
 
-                    Label(m,text='PATIENT ID').grid(row=7)
-                    user=Entry(m).grid(row=7,column=1)
-                    Label(m,text='Enter PATIENT NAME:').grid(row=8)
-                    name=Entry(m).grid(row=8,column=1)
-                    Label(m,text='Enter PATIENT AGE:').grid(row=9)
-                    age=Entry(m).grid(row=9,column=1)
-                    Label(m,text='Enter CONSULTING DOCTOR:').grid(row=10)
-                    doc=Entry(m).grid(row=10,column=1)
-                    G= str(datetime.date.today())
+                    Label(m,text='PATIENT ID').grid(row=10)
+                    user=Entry(m).grid(row=10,column=1)
+                    Label(m,text='Enter PATIENT NAME:').grid(row=11)
+                    name=Entry(m).grid(row=11,column=1)
+                    Label(m,text='Enter PATIENT AGE:').grid(row=12)
+                    age=Entry(m).grid(row=12,column=1)
+                    Label(m,text='Enter CONSULTING DOCTOR:').grid(row=13)
+                    doc=Entry(m).grid(row=13,column=1)
                     
                     
                     
                     
                     
                     
-                    Button(m,text='PATIENT ENTRY DONE',command=send)
-
+                    
+                    Button(m,text='PATIENT ENTRY DONE',command=send).grid(row=14,column=3)
+                def send1():
+                    user1=user.get()
+                    
+                    age1=age.get()
+                    a.execute("select distinct* from PATIENT where PATIENT_ID="+str(user) )
+                    D=a.fetchall()             
+                    a.execute("insert into PATIENT values("+str(user)+",'"+D[0][1]+"',"+str(age)+",'"+str(datetime.date.today())+"','"+D[0][4]+"',NULL,NULL,NULL,NULL,NULL,NULL)")
                 def rcpde():
-                    Label(m,text='PATIENT ID').grid(row=7)
-                    user=Entry(m).grid(row=7,column=1)
-                    Label(m,text='Enter PATIENT NAME:').grid(row=8)
-                    name=Entry(m).grid(row=8,column=1)
-                    Label(m,text='Enter PATIENT AGE:').grid(row=9)
-                    age=Entry(m).grid(row=9,column=1)
-                    Label(m,text='Enter CONSULTING DOCTOR:').grid(row=10)
-                    doc=Entry(m).grid(row=10,column=1)
-                    G= str(datetime.date.today())
+                    Label(m,text='Enter PATIENT ID:').grid(row=15)
+                    user=Entry(m).grid(row=15,column=1)
+                    
+                    
+                    Label(m,text='Enter PATIENT AGE').grid(row=16)
+                    user=Entry(m).grid(row=16,column=1)
+                    Button(m,text='DONE',command=send1).grid(row=117,column=3)
 
-
+                def send2():
+                    user1=user.get()
+                    a.execute("select PATIENT_ID  ,PATIENT_NAME,AGE,DATE_OF_LAST_VISIT,CONSULTING_DOCTOR from PATIENT where PATIENT_ID="+str(user))
+                    D=a.fetchall()
+                    data=pd.DataFrame(D,columns=['PATIENT_ID ' ,'PATIENT_NAME','AGE','DATE_OF_LAST_VISIT','CONSULTING_DOCTOR'])
+                    data.set_index('PATIENT_ID ',inplace=True)
+                    print('-'*100)
+                    print(data.iloc[:,0:5])
+                    print('-'*100)
                 def vpd():
-                    Label(m,text='PATIENT ID').grid(row=7)
-                    user=Entry(m).grid(row=7,column=1)
-                    Label(m,text='Enter PATIENT NAME:').grid(row=8)
-                    name=Entry(m).grid(row=8,column=1)
-                    Label(m,text='Enter PATIENT AGE:').grid(row=9)
-                    age=Entry(m).grid(row=9,column=1)
-                    Label(m,text='Enter CONSULTING DOCTOR:').grid(row=10)
-                    doc=Entry(m).grid(row=10,column=1)
-                    G= str(datetime.date.today())
-
+                    Label(m,text='PATIENT ID').grid(row=18)
+                    user=Entry(m).grid(row=18,column=1)
+                    Button(m,text='Display',command=send2).grid(row=19,column=1)
+                def send3():
+                    user1=user.get()
+                    a.execute("delete from patient where PATIENT_ID="+str(user))
                 def delre():
-                    Label(m,text='PATIENT ID').grid(row=7)
-                    user=Entry(m).grid(row=7,column=1)
-                    Label(m,text='Enter PATIENT NAME:').grid(row=8)
-                    name=Entry(m).grid(row=8,column=1)
-                    Label(m,text='Enter PATIENT AGE:').grid(row=9)
-                    age=Entry(m).grid(row=9,column=1)
-                    Label(m,text='Enter CONSULTING DOCTOR:').grid(row=10)
-                    doc=Entry(m).grid(row=10,column=1)
-                    G= str(datetime.date.today())
-
+                    Label(m,text='Enter PATIENT ID which has to deleted:').grid(row=7)
+                    user=Entry(m).grid(row=20,column=1)
+                     
                 Button(m,text='1.NEW PATIENT',command= newp).grid(row=5)
                 Button(m,text='2.ROUTINE CHECKUP PATIENT DETAILS ENTRY',command=rcpde).grid(row=6)
                 Button(m,text='3.VIEW PATIENT DETAILS',command=vpd).grid(row=7)
                 Button(m,text='4.DELETE RECORD',command= delre).grid(row=8)
-                Button(m,text='5.GO BACK').grid(row=9)
-               
-                            
-                
-                
-                    
-                    
-                    
-                     
-                     
-
-                   
-                
-               
-                   
-
-                    
-                    
+                Button(m,text='5.GO BACK',command=receptionist).grid(row=9)          
+                                    
+                                                                                                                            
+              
+                 
+                  
                
 def general():
                 def upep():
@@ -113,10 +107,6 @@ def general():
                 Button(m,text='3.DELETE RECORD',command=dr).grid(row=7)
                 Button(m,text='4.GO BACK').grid(row=8)
                 
-                print('1.UPDATE PATIENTS EYE POWER')
-                print('2.STATISTICS OF PATIENT')
-                print('3.DELETE RECORD')
-                print('4.GO BACK')
                
                       
                         
@@ -141,10 +131,6 @@ def doctor():
                 Button(m,text='2.DELETE RECORD',command=delr).grid(row=6)
                 Button(m,text='3.GIVE PRESCRIPTION',command=givpre).grid(row=7)
                 Button(m,text='4.GO BACK').grid(row=8)
-                print('1.CHECK COMPLETE RECORD')
-                print('2.DELETE RECORD')
-                print('3.GIVE PRESCRIPTION')
-                print('4.GO BACK')
                 
                 
                 
